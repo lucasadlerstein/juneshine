9137b3dc2acfa9029bdcd23899f8506a6532d258
lucasadlerstein <lucasadlerstein@gmail.com>
2020-10-20 16:27:06 -0300

    First spinner

--- src/components/Quiz/QuizComponent.js
+++ src/components/Quiz/QuizComponent.js
@@ -12,6 +12,7 @@ import ResultComponent from "./ResultComponent"
 import IntroSlide from './IntroSlide';
 import styled from 'styled-components';
 import EmailKlaviyo from './EmailKlaviyo';
+import '../../styles/spinner.css';
 
 const FlexOption = styled(Flex)`
     margin-left: auto!important;
@@ -43,12 +44,16 @@ const QuizComponent = ({items, data, answers, numberOfSteps}) => {
     const { step, hide, setNumberOfSteps, totalSteps } = QuizContext;
     
     const [changedSteps, setChangedSteps] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         if(changedSteps === false) {
             setNumberOfSteps(numberOfSteps);
             setChangedSteps(true);
         }
+        setTimeout(() => {
+            setLoading(false);
+        }, 700);
         const quizSection = document.querySelector('#quizSection'); 
         if(quizSection) {
             quizSection.style.transition = 'all .3s ease';
@@ -67,52 +72,59 @@ const QuizComponent = ({items, data, answers, numberOfSteps}) => {
                 { (step === totalSteps) ? null : <StepIndicator /> }
 
                 {/* HERE STARTS EACH STEP */}
-
                 {
-                    (step === -3) ? <IntroSlide /> : (
+                    (loading === true) ? (
+                        <div className="spinner">
+                            <div className="bounce1"></div>
+                            <div className="bounce2"></div>
+                            <div className="bounce3"></div>
+                        </div>
+                    ) : (
+                        (step === -3) ? <IntroSlide /> : (
                         
-                        (step === -2) ? (
-                            <GetName />
-                        ) : (
-
-                            (step === -1) ? <EmailKlaviyo /> : (
-
-                                (step >= data.length) ? (
-
-                                    // (step === data.length) ? // ! This line is the email at the end
-                                    // <EmailKlaviyo />         // ! This line is the email at the end
-                                    // :                        // ! This line is the email at the end
-                                    <ResultComponent items={items} answers={answers} />
-
-                                ) : (
-                                    <div id="quizSection" key={step}>
-                                        <Question lineOne={data[step].Question.LineOne} lineTwo={data[step].Question.LineTwo} />
-                                        <Flex sx={{width: '100%', justifyContent: 'center',  flexWrap: 'wrap', textAlign: 'center', margin: '0 auto 50px auto'}}>
-                                        {
-                                            data[step].Answers.map( one => {
-                                                if(one.Type === 'text') {
-                                                    return (
-                                                        <FlexOption key={one.Value}>
-                                                            <TextAnswer text={one.Text} value={one.Value} icon={one.Icon} />
-                                                        </FlexOption>
-                                                    )
-                                                } 
-                                                
-                                                if(one.Type === 'darkimg' || one.Type === 'lightimg') {
-                                                    return (
-                                                        <FlexOption key={one.Value}>
-                                                            <ImgAnswer img={one.Picture} mode={one.Type} text={one.Text} value={one.Value} />
-                                                        </FlexOption>
-                                                    )
-                                                }
-                                            })
-                                        }
-                                        </Flex>
-                                    </div>
+                            (step === -2) ? (
+                                <GetName />
+                            ) : (
+    
+                                (step === -1) ? <EmailKlaviyo /> : (
+    
+                                    (step >= data.length) ? (
+    
+                                        // (step === data.length) ? // ! This line is the email at the end
+                                        // <EmailKlaviyo />         // ! This line is the email at the end
+                                        // :                        // ! This line is the email at the end
+                                        <ResultComponent items={items} answers={answers} />
+    
+                                    ) : (
+                                        <div id="quizSection" key={step}>
+                                            <Question lineOne={data[step].Question.LineOne} lineTwo={data[step].Question.LineTwo} />
+                                            <Flex sx={{width: '100%', justifyContent: 'center',  flexWrap: 'wrap', textAlign: 'center', margin: '0 auto 50px auto'}}>
+                                            {
+                                                data[step].Answers.map( one => {
+                                                    if(one.Type === 'text') {
+                                                        return (
+                                                            <FlexOption key={one.Value}>
+                                                                <TextAnswer text={one.Text} value={one.Value} icon={one.Icon} />
+                                                            </FlexOption>
+                                                        )
+                                                    } 
+                                                    
+                                                    if(one.Type === 'darkimg' || one.Type === 'lightimg') {
+                                                        return (
+                                                            <FlexOption key={one.Value}>
+                                                                <ImgAnswer img={one.Picture} mode={one.Type} text={one.Text} value={one.Value} />
+                                                            </FlexOption>
+                                                        )
+                                                    }
+                                                })
+                                            }
+                                            </Flex>
+                                        </div>
+                                    )
                                 )
                             )
-                        )
-                    )   
+                        )  
+                    )
                 }
                 {/* END OF STEPS */}
 
